06cfcd9cf22f349b21a5bd85cc3ba902c3739d6e
Carwin Young <carwin.young@snyk.io>
2021-10-18 16:43:33 -0500

    Shuffling around the response to cloudformation.

--- lambda_functions/source/AddSnykIntegration/lambda_function.js
+++ lambda_functions/source/AddSnykIntegration/lambda_function.js
@@ -1,20 +1,23 @@
+// import https from 'https';
 const https = require('https');
 const url = require('url')
 const request = require('request');
 
-// The CloudFormation template provides a number of inputs which
-// are passed to the Lambda as environment variables. Set them
-// to variables to avoid typing process.env all the time.
-const orgID = process.env.orgId;
+// Inputs:
+//  - The CloudFormation template provides a number of inputs which
+//    are passed to the Lambda as environment variables. Set them
+//    to variables to avoid typing process.env all the time.
+const groupId = process.env.groupId;
+const sourceOrgId = process.env.orgId;
 const authToken = process.env.authToken;
+const orgPatternPrefix = process.env.orgPatternPrefix;
 const awsRegion = process.env.awsRegion;
 const awsRoleArn = process.env.awsRoleArn;
 
 // Set up some messages:
 const messages = {
-  integrationExists: `An existing ECR integration already exists for the provided Snyk organization: ${orgID}.`,
-  integrationCreated: `Successfully created ECR integration for Snyk organization ${orgID}`,
-  problemCreatingIntegration: `A problem occurred while attempting to create an ECR integration in Snyk for organization ${orgID}.`,
+  integrationCreated: 'Successfully created ECR integration for Snyk organization %s',
+  problemCreatingIntegration: 'A problem occurred while attempting to create an ECR integration in Snyk for organization %s.',
   APIGetError: 'Error during GET request to Snyk API.',
   APIPostError: 'Error during POST request to Snyk API.',
   notCreateEvent: 'Skipping Snyk integration creation... This is not a CloudFormation CREATE event.'
@@ -26,77 +29,124 @@ const messages = {
 // when the Lambda is invoked. In the CloudFormation template(s) this
 // is specified in the Properties section of AddSnykIntegrationLambdaFunction.
 exports.handler = (event, context) => {
+
+  let creationStatus = 'FAILED';
+  let creationData = { message: 'no data' };
+
   // Only act on Create.
   if (event.RequestType === 'Create') {
     try {
-      // First do a GET on the organization's integrations to see if there's an ECR
-      // integration already, if there is, we need to skip.
 
-      // request and axios are easier to chain off of than pure https, but it could be done.
+      // Outline:
+      // - First Generate a name we'll use for a new org.
+      // - @TODO: We optionally take a SOURCE ORG ID to generate a new Org.
+      // - Next, query the Snyk API to create a new organization.
+      // - Collect the returned ID of the org from the last query
+      // - Make a second request to create the integration in the new Org.
+      //   Since the org is new there's no need to check for existing integrations.
+
+      // Generate a new Organization name.
+      const newOrgNameRand = Math.random().toString(36).slice(2)
+      const newOrgName = `${orgPatternPrefix}-${newOrgNameRand}`;
+
+      console.log('New Org being created: ', newOrgName);
+
+
+      // Create the new Organization in Snyk via the Snyk API.
       request({
-        method: 'GET',
-        url: `https://snyk.io/api/v1/org/${orgID}/integrations`,
+        method: 'POST',
+        url: `https://snyk.io/api/v1/org`,
         headers: {
           'Content-Type': 'application/json; charset=utf-8',
           'Authorization': `token ${authToken}`
-        }
+        },
+        body: JSON.stringify({
+          name: newOrgName,
+          groupId: groupId,
+          sourceOrgId: sourceOrgId
+        })
       }, (error, response, body) => {
+        console.log('create new org - Response Status: ', response.statusCode);
+        console.log('create new org - Headers: ', JSON.stringify(response.headers));
+        console.log('create new org - Response:', body);
 
-        if (!error) {
-          const data = response.body;
-          let existingECRIntegration = false;
-
-          if (data.ecr && data.ecr !== '') {
-            existingECRIntegration = true;
-          }
-
-          // If there is no existing ECR Integration for this organization within Snyk,
-          // it is safe to create one.
-          if (existingECRIntegration === false) {
-            request({
-              method: 'POST',
-              url: `https://snyk.io/api/v1/org/${orgID}/integrations`,
-              headers: {
-                'Content-Type': 'application/json; charset=utf-8',
-                'Authorization': `token ${authToken}`
-              },
-              body: "{  \"type\": \"ecr\",  \"credentials\": {    \"region\": \"" + awsRegion + "\",    \"roleArn\": \"" + awsRoleArn + "\"  }}"
-            }, (error, response, body) => {
-              console.log('response status', response.statusCode);
-              if (response.statusCode === 200 || response.statusCode === 201) {
-                console.log(messages.integrationCreated);
-                sendResponse(event, context, 'SUCCESS', JSON.parse(body));
-              } else {
-                let message;
-                response.statusCode === 409 ? message = messages.integrationExists : message = messages.problemCreatingIntegration;
-                sendResponse(event, context, 'FAILED', error !== null ? {message: error} : {message});
-              }
-            });
-          } else {
-            // The GET request returned an existing ECR integration with Snyk.
-            // Tell CloudFormation we've failed.
-            sendResponse(event, context, 'FAILED', {message: messages.integrationExists});
-          }
-
-        } else {
-          // An error was received from the GET request. Shut it down.
-          console.log(messages.APIGetError, error);
-          sendResponse(event, context, 'FAILED', {message: error});
+        if (error) {
+          console.log('Error received when sending POST to Snyk API', error);
+          sendResponse(event, context, 'FAILED', error);
         }
 
+        // Successfully created the new Organization.
+        if (response.statusCode === 201) {
+
+          const newOrg = JSON.parse(body);
+          const newOrgId = newOrg.id;
+
+          console.log('body before integration', body);
+
+
+
+          console.log(`Requesting new integration creation at: https://snyk.io/api/v1/org/${newOrgId}/integrations`);
+
+          // Now that we have an org we need to send a request for creating
+          // an integration, using the new org's ID.
+          request({
+            method: 'POST',
+            url: `https://snyk.io/api/v1/org/${newOrgId}/integrations`,
+            headers: {
+              'Content-Type': 'application/json; charset=utf-8',
+              'Authorization': `token ${authToken}`
+            },
+            body: "{  \"type\": \"ecr\",  \"credentials\": {    \"region\": \"" + awsRegion + "\",    \"roleArn\": \"" + awsRoleArn + "\"  }}"
+          }, (error, response, body) => {
+
+            console.log('create new integration - Response Status: ', response.statusCode);
+            console.log('create new integration - Headers: ', JSON.stringify(response.headers));
+            console.log('create new integration - Response:', body);
+
+            if (!error && (response.statusCode === 200 || response.statusCode === 201)) {
+              console.log(messages.integrationCreated);
+              creationStatus = 'SUCCESS';
+              creationData = JSON.parse(body);
+              // sendResponse(event, context, 'SUCCESS', JSON.parse(body));
+            } else {
+              console.log('Problem creating new Integration for Org: ', newOrgId);
+              if (error) {
+                console.log('The error encountered was: ', error);
+              }
+              creationStatus = 'FAILED';
+              creationData = JSON.parse(body);
+              // sendResponse(event, context, 'FAILED', error !== null ? { message: error } : { message: JSON.parse(body) });
+            }
+          });
+        } else {
+          // Else we didn't get a 201 when trying to create the org
+          // sendResponse(event, context, 'FAILED', body);
+          creationStatus = 'FAILED';
+          creationData = JSON.parse(body);
+        }
       });
 
 
-    } catch(e) {
+    } catch (e) {
       console.log('Error during API call:\n', e);
-      sendResponse(event, context, 'FAILED', {message: e});
+      // sendResponse(event, context, 'FAILED', { message: e });
+      creationStatus = 'FAILED';
+      creationData = { message: e };
     }
   } else {
     // If CloudFormation isn't sending this function a Create event, succeed with
     // a note saying the integration creation has been skipped.
     // It may be a good idea to remove the integration that exists on a tear-down event.
-    sendResponse(event, context, 'SUCCESS', {message: messages.notCreateEvent});
+    // sendResponse(event, context, 'SUCCESS', { message: messages.notCreateEvent });
+    creationStatus = 'SUCCESS';
+    creationData = { message: messages.notCreateEvent };
   }
+
+
+
+  // Once everything is finished, send the response.
+  sendResponse(event, context, creationStatus, creationData);
+
 }
 
 // Send response to pre-signed URL
